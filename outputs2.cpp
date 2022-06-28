323e4ea4ea96870d1f3ac0d2ab0d277313fdc11f
Anushka mudgal <105125740+its-me-anushka@users.noreply.github.com>
2022-06-28 20:50:25 +0530

    Create outputs2.cpp

---
+++ outputs2.cpp
@@ -0,0 +1,21 @@
+// What is the output
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int var1 = 5;
+    int var2 = 6;
+    cout << (var1 > var2);
+ }
+
+/* 
+Options
+This problem has only one correct answer
+true
+false
+0 
+1
+Correct Answer
+Solution Description 
+'>' operator gives a boolean answer. And the condition is false. So answer will be false and 0 will be printed (boolean variables print only 0 and 1). */ 
