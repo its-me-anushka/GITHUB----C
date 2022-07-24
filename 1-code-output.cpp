970e478ddc93c6bea5f50956193075d73e150e6c
Anushka mudgal <105125740+its-me-anushka@users.noreply.github.com>
2022-07-24 21:07:13 +0530

    Create 1-code-output.cpp

---
+++ 1-code-output.cpp
@@ -0,0 +1,14 @@
+ Fill the output
+
+What will be the output of the following code ?
+void doubleValue(int a) {
+    a = a * 2;
+}
+int main() {
+    int a = 8;
+    doubleValue(a);
+    cout << a;
+}
+
+
+// Ans - 8 
